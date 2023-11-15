6539a3158686b4a3ff585c1ce188ee8a066b9b6e
simon-zimmermann <132376719+simon-zimmermann@users.noreply.github.com>
2023-11-15 16:12:50 +0100

    more sensory

--- visualize_current_data.m
+++ visualize_current_data.m
@@ -14,10 +14,14 @@ stackedplot(data_raw,["imu2_accX","imu2_accY","imu2_accZ"],"XVariable","time")
 title("Acceleration data IMU2")
 
 figure()
-stackedplot(data_raw,["tempA0","tempA1"],"XVariable","time")
+stackedplot(data_raw,["temp0","temp1", "temp2", "temp3"],"XVariable","time")
 title("Temperature")
 
 figure()
 stackedplot(data_raw,["power1Volt","power1MilliAmp","power1MilliWatt","power1ShuntMilliVolt"],"XVariable","time")
-title("Power Meter")
+title("Power Meter 1")
+
+figure()
+stackedplot(data_raw,["power2Volt","power2MilliAmp","power2MilliWatt","power2ShuntMilliVolt"],"XVariable","time")
+title("Power Meter 2")
 
