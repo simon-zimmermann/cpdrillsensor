da6322d5c91c25043ce7c137f4f6ffc71782dd85
Simon <132376719+simon-zimmermann@users.noreply.github.com>
2023-11-15 16:12:32 +0100

    added ntc logic; may be wrong...

--- logger/__main__.py
+++ logger/__main__.py
@@ -6,6 +6,7 @@ from oclock import Timer
 from grove.adc import ADC
 from ina219 import INA219
 from ina219 import DeviceRangeError
+from thermistor_utils import SH_converter
 
 from lsm6ds3 import LSM6DS3
 
@@ -54,8 +55,10 @@ def main():
 
     # amp meter (see https://pypi.org/project/pi-ina219/)
     SHUNT_OHMS = 0.1
-    ampMeter1 = INA219(SHUNT_OHMS)
+    ampMeter1 = INA219(SHUNT_OHMS, address=0x40)
     ampMeter1.configure()
+    ampMeter2 = INA219(SHUNT_OHMS, address=0x41)
+    ampMeter2.configure()
 
     # file stup
     datetime = time.strftime("%Y-%m-%d_%H-%M-%S")
@@ -68,13 +71,22 @@ def main():
             imu1_gyroX, imu1_gyroY, imu1_gyroZ, \
             imu2_accX, imu2_accY, imu2_accZ, \
             imu2_gyroX, imu2_gyroY, imu2_gyroZ,\
-            tempA0, tempA1,\
-            power1Volt, power1MilliAmp, power1MilliWatt, power1ShuntMilliVolt\n")
+            temp0, temp1,temp2,temp3, \
+            power1Volt, power1MilliAmp, power1MilliWatt, power1ShuntMilliVolt,\
+            power2Volt, power2MilliAmp, power2MilliWatt, power2ShuntMilliVolt\n")
 
     # the second the file was created
     file_startsecond = time.time()
     loopsecond = 0
     logCount = 0
+
+    # steinhart-hart converter (see ntc datasheet)
+    readings = (
+        (0, 32650),
+        (25, 10000),
+        (50, 3603),
+    )
+    conv = SH_converter.from_points(readings)
 
     # loop timer
     timer = Timer(interval=1 / logFrequency)
@@ -95,25 +107,41 @@ def main():
             gyroData2 = lsm2.getGyroData()
             gyroString2 = "%.6f,%.6f,%.6f" % (gyroData2[0], gyroData2[1], gyroData2[2])
 
-            # get temperature value
-            temp0 = adc.read_voltage(0)
-            if temp0 > 3300:
-                temp0 = 1400
-            temp1 = adc.read_voltage(1)
-            if temp1 > 3300:
-                temp1 = 1400
-            tempString = "%d,%d" % (temp0, temp1)
+            # get temperature  resistance value
+            # 10kOhm NTC probe; 10kOhm resistor as voltage divider
+            # Vntc = Vcc * Rntc / (Rntc + 10kOhm)
+            # Vread = Vntc / Vcc
+            # Vread = Rntc / (Rntc + 10kOhm)
+            # Rntc = Vread * 10kOhm / (1 - Vread)
+            #TODO MATH MY BE WRONG!
+            vread0 = adc.read(0)
+            vread1 = adc.read(1)
+            vread2 = adc.read(2)
+            vread3 = adc.read(3)
+            rntc0 = vread0 * 10000 / (1 - vread0)
+            rntc1 = vread1 * 10000 / (1 - vread1)
+            rntc2 = vread2 * 10000 / (1 - vread2)
+            rntc3 = vread3 * 10000 / (1 - vread3)
+            temp0 = conv.temperature(-rntc0)
+            temp1 = conv.temperature(-rntc1)
+            temp2 = conv.temperature(-rntc2)
+            temp3 = conv.temperature(-rntc3)
+
+            #tempString = "%.1f,%.1f,%.1f,%.1f" % (rntc0, rntc1, rntc2, rntc3)
+            tempString = "%.1f,%.1f,%.1f,%.1f" % (temp0, temp1, temp2, temp3)
 
             # get power meter
             powerString1 = "%.3f,%.3f,%.3f,%.3f" % (
                 ampMeter1.voltage(), ampMeter1.current(), ampMeter1.power(), ampMeter1.shunt_voltage())
+            powerString2 = "%.3f,%.3f,%.3f,%.3f" % (
+                ampMeter2.voltage(), ampMeter2.current(), ampMeter2.power(), ampMeter2.shunt_voltage())
 
             # Duration the data aquisition took
             aqTime = time.perf_counter() - startTime
 
             # Write data to file
-            f.write("%.3f,%.6f,%s,%s,%s,%s,%s,%s\n" %
-                    (loopsecond, aqTime, accString1, gyroString1, accString2, gyroString2, tempString, powerString1))
+            f.write("%.3f,%.6f,%s,%s,%s,%s,%s,%s,%s\n" %
+                    (loopsecond, aqTime, accString1, gyroString1, accString2, gyroString2, tempString, powerString1, powerString2))
 
             # display once every 2 seconds how much data has been logged
             logCount = logCount + 1
